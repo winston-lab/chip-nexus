winston-lab/chip-nexus

commit 701602b8d56891aa9a1a1718397912f8bdcaece1

Fix bug in midpoint coverage resulting from taking unionbedg of bedgraphs with overlapping annotations due to shifting at end of chromosomes. This resulted in certain sections of coverage being lost.

rules/chip-nexus_genome_coverage.smk
@@ -63,8 +63,8 @@ rule midpoint_coverage:
                 awk '{{count[NR]=$1;}} END{{if (NR % 2) {{print count[(NR+1)/2]/2.0}} else {{print (count[(NR/2)] + count[(NR/2)+1]) / 4.0;}} }}' | \
                 xargs printf "%.*f\n" 0)
         (bedtools unionbedg -i \
-                <(bedtools shift -i {input.plus} -g <(faidx {input.fasta} -i chromsizes) -s $half_median_fragsize) \
-                <(bedtools shift -i {input.minus} -g <(faidx {input.fasta} -i chromsizes) -s -$half_median_fragsize) -g <(faidx {input.fasta} -i chromsizes) -empty | \
+                <(bedtools shift -i {input.plus} -g <(faidx {input.fasta} -i chromsizes) -s $half_median_fragsize | bedtools groupby -g 1,2,3 -c 4 -o sum) \
+                <(bedtools shift -i {input.minus} -g <(faidx {input.fasta} -i chromsizes) -s -$half_median_fragsize) -g <(faidx {input.fasta} -i chromsizes | bedtools groupby -g 1,2,3 -c 4 -o sum) -empty | \
          awk 'BEGIN{{FS=OFS="\t"}}{{print $1, $2, $3, $4+$5}}' > {output}) &> {log}
         """
 

commit 195be7b068903eb011c461fcdc262e84b10abb9e

Fix bug in midpoint coverage introduced in 701602b8d56891aa9a1a1718397912f8bdcaece1

rules/chip-nexus_genome_coverage.smk
@@ -64,7 +64,7 @@ rule midpoint_coverage:
                 xargs printf "%.*f\n" 0)
         (bedtools unionbedg -i \
                 <(bedtools shift -i {input.plus} -g <(faidx {input.fasta} -i chromsizes) -s $half_median_fragsize | bedtools groupby -g 1,2,3 -c 4 -o sum) \
-                <(bedtools shift -i {input.minus} -g <(faidx {input.fasta} -i chromsizes) -s -$half_median_fragsize) -g <(faidx {input.fasta} -i chromsizes | bedtools groupby -g 1,2,3 -c 4 -o sum) -empty | \
+                <(bedtools shift -i {input.minus} -g <(faidx {input.fasta} -i chromsizes) -s -$half_median_fragsize | bedtools groupby -g 1,2,3 -c 4 -o sum) -g <(faidx {input.fasta} -i chromsizes) -empty | \
          awk 'BEGIN{{FS=OFS="\t"}}{{print $1, $2, $3, $4+$5}}' > {output}) &> {log}
         """
 